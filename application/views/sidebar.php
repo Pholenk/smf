7aa11a4ca46f9eeda3bb03188a0252899bd4990d
Pholenk <pholenkadi17@gmail.com>
2017-04-24 15:54:02 +0700

    modified sidebar

--- application/views/sidebar.php
+++ application/views/sidebar.php
@@ -4,7 +4,7 @@
 		<!-- Sidebar user panel -->
 		<div class="user-panel">
 			<div class="pull-left image">
-				<img src="vendor/almasaeed-2010/adminlte/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+				<img src="/vendor/almasaeed2010/adminlte/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
 			</div>
 			<div class="pull-left info">
 				<p>Alexander Pierce</p>
