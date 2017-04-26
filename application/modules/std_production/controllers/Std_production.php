5b64346f446a1393e1c8ce611da64ea0ab360996
Pholenk <pholenkadi17@gmail.com>
2017-04-26 09:23:26 +0700

    #13 - modified std_production module and browse page

--- application/modules/std_production/controllers/Std_production.php
+++ application/modules/std_production/controllers/Std_production.php
@@ -5,14 +5,14 @@
 */
 class Std_production extends MX_Controller
 {
-	private $is_login;
+	private $_access;
 
 	function __construct()
 	{
 		parent::__construct();
 		$this->load->module('auth');
 		$this->load->model('ProductionModel');
-		$this->is_login = $this->auth->is_login();
+		$this->_access = $this->auth->privileges_read('standard');
 	}
 
 	/**
@@ -20,26 +20,18 @@ class Std_production extends MX_Controller
 	 */
 	public function index()
 	{
-		$this->browse();
+		$this->_browse();
 	}
 
 	/**
 	 * browse method
 	 */
-	public function browse()
+	function _browse()
 	{
-		if ($this->is_login)
-		{
-			$access = $this->auth->privileges_read('standard_browse');
-			if ($access)
-			{
-				$data['production_data'] = $this->ProductionModel->browse();
-				$this->_show_interface('browse', $data);
-			}
-			else
-			{
-				$this->_show_interface('Unauthorize', '');
-			}
+		if ($this->_access)
+		{			
+			$data['production_data'] = $this->ProductionModel->browse();
+			$this->_show_interface('browse', $data);
 		}
 		else
 		{
@@ -50,66 +42,62 @@ class Std_production extends MX_Controller
 	/**
 	 * read method
 	 */
-	public function read($id)
+	public function read($id = '')
 	{
-		if ($this->is_login)
+		if ($this->_access && !empty($id))
 		{
-			$access = $this->auth->privileges_read('standard_edit');
-			if ($access && !empty($id))
+			$production_read_data = $this->ProductionModel->read($id);
+			foreach ($production_read_data as $data) 
 			{
-				$production_read_data = $this->ProductionModel->read($id);
-				foreach ($production_read_data as $data) 
-				{
-					echo "
-					<div class='modal-header'>
-					<h1 class='modal-title'>Edit Standard Production</h1>
-					</div>
-					<form class='form-horizontal' method='post'  id='edit_form_std_prod'>
-					<div class='modal-body'>
-					<div class='form-group'>
-					<label class='col-xs-4 control-label'>Berat</label>
-					<div class='col-xs-7'>
-					<input name='berat_badan' id='berat_edit' type='number' step=0.0001 min=0 class='form-control' value='".$data->berat_badan."'required>
-					</div>
-					</div>
-					<div class='form-group'>
-					<label class='col-xs-4 control-label'>Feed Convertion Ratio</label>
-					<label class='col-xs-7 control-label' style='text-align:left;' id='fcr_edit'>".$data->fcr."</label>
-					</div>
-					<div class='form-group'>
-					<label class='col-xs-4 control-label'>Mortalitas</label>
-					<div class='col-xs-7'>
-					<input name='mortalitas' id='mortalitas_edit' type='number' step=0.0001 min=0 class='form-control' value='".$data->mortalitas."'required>
-					</div>
-					</div>
-					<div class='form-group'>
-					<label class='col-xs-4 control-label'>Feed Intake</label>
-					<div class='col-xs-7'>
-					<input name='feed' id='feed_edit' type='number' step=0.0001 min=0 class='form-control' value='".$data->feed_intake."'required>
-					</div>
-					</div>
-					<div class='form-group'>
-					<label class='col-xs-4 control-label'>Umur</label>
-					<div class='col-xs-7'>
-					<input name='age' id='age_edit' type='number' step=0.0001 min=0 class='form-control' value='".$data->age."'required>
-					</div>
-					</div>
-					</div>
-					<div class='modal-footer'>
-					<div class='col-xs-6'>
-					<button class='btn btn-success' type='submit' id='save_edit_std_prod'><i class='fa fa-save'></i> Save</button>
-					</div>
-					<div class='col-xs-6 push-left'>
-					<button class='btn btn-danger push-left' type='button' data-dismiss='modal'><i class='fa fa-times'></i> Cancel</button>
-					</div>
-					</div>
-					</form>";
-          		}
-			}
-			else
-			{
-				$this->_show_interface('Unauthorize', '');
-			}
+				echo "
+				<div class='modal-header'>
+				<h1 class='modal-title'>Edit Standard Production</h1>
+				</div>
+				<form class='form-horizontal' method='post'  id='edit_form_std_prod'>
+				<div class='modal-body'>
+				<div class='form-group'>
+				<label class='col-xs-4 control-label'>Berat</label>
+				<div class='col-xs-7'>
+				<input name='berat_badan' id='berat_edit' type='number' step=0.0001 min=0 class='form-control' value='".$data->berat_badan."'required>
+				</div>
+				</div>
+				<div class='form-group'>
+				<label class='col-xs-4 control-label'>Feed Convertion Ratio</label>
+				<label class='col-xs-7 control-label' style='text-align:left;' id='fcr_edit'>".$data->fcr."</label>
+				</div>
+				<div class='form-group'>
+				<label class='col-xs-4 control-label'>Mortalitas</label>
+				<div class='col-xs-7'>
+				<input name='mortalitas' id='mortalitas_edit' type='number' step=0.0001 min=0 class='form-control' value='".$data->mortalitas."'required>
+				</div>
+				</div>
+				<div class='form-group'>
+				<label class='col-xs-4 control-label'>Feed Intake</label>
+				<div class='col-xs-7'>
+				<input name='feed' id='feed_edit' type='number' step=0.0001 min=0 class='form-control' value='".$data->feed_intake."'required>
+				</div>
+				</div>
+				<div class='form-group'>
+				<label class='col-xs-4 control-label'>Umur</label>
+				<div class='col-xs-7'>
+				<input name='age' id='age_edit' type='number' step=0.0001 min=0 class='form-control' value='".$data->age."'required>
+				</div>
+				</div>
+				</div>
+				<div class='modal-footer'>
+				<div class='col-xs-6'>
+				<button class='btn btn-success' type='submit' id='save_edit_std_prod'><i class='fa fa-save'></i> Save</button>
+				</div>
+				<div class='col-xs-6 push-left'>
+				<button class='btn btn-danger push-left' type='button' data-dismiss='modal'><i class='fa fa-times'></i> Cancel</button>
+				</div>
+				</div>
+				</form>";
+      		}
+		}
+		elseif ($this->_access && empty($id))
+		{
+			redirect(base_url('/std_production'));
 		}
 		else
 		{
@@ -120,31 +108,27 @@ class Std_production extends MX_Controller
 	/**
 	 * edit method
 	 */
-	public function edit($id)
+	public function edit($id = '')
 	{
-		if ($this->is_login)
+		if ($this->_access && !empty($id))
 		{
-			$access = $this->auth->privileges_read('standard_edit');
-			if ($access && !empty($id))
-			{
-				$fcr = $this->input->post('feed') / ($this->input->post('berat_badan') * 1000);
-				$production_data_edit = array(
-					'berat_badan' => $this->input->post('berat_badan'),
-					'mortalitas' => $this->input->post('mortalitas'),
-					'feed_intake' => $this->input->post('feed'),
-					'age' => $this->input->post('age'),
-					'fcr' => $fcr,
-					);
-				echo ($this->ProductionModel->edit($id, $production_data_edit) ? 'SUCCESS' : '!SUCCESS');
-			}
-			else
-			{
-				$this->_show_interface('Unauthorize', '');
-			}
+			$fcr = $this->input->post('feed') / ($this->input->post('berat_badan') * 1000);
+			$production_data_edit = array(
+				'berat_badan' => $this->input->post('berat_badan'),
+				'mortalitas' => $this->input->post('mortalitas'),
+				'feed_intake' => $this->input->post('feed'),
+				'age' => $this->input->post('age'),
+				'fcr' => $fcr,
+				);
+			echo ($this->ProductionModel->edit($id, $production_data_edit) ? 'SUCCESS' : '!SUCCESS');
+		}
+		elseif ($this->_access && empty($id))
+		{
+			redirect(base_url('/std_production'));
 		}
 		else
 		{
-			redirect(base_url());
+			echo "!LOGIN";
 		}
 	}
 
@@ -153,99 +137,79 @@ class Std_production extends MX_Controller
 	 */
 	public function add()
 	{
-		if ($this->is_login)
+		if ($this->_access && empty($this->input->post('berat_badan')))
 		{
-			$access = $this->auth->privileges_read('standard_add');
-			if ($access)
-			{
-				if (empty($this->input->post('berat_badan')))
-				{
-					echo "
-					<div class='modal-header'>
-					<h1 class='modal-title'>Add Standard Production</h1>
-					</div>
-					<form class='form-horizontal' method='post' id='add_form_std_prod'>
-					<div class='modal-body'>
-					<div class='form-group'>
-					<label class='col-xs-4 control-label'>Berat</label>
-					<div class='col-xs-7'>
-					<input name='berat_badan' id='berat_add' type='number' step=0.0001 min=0 class='form-control' required>
-					</div>
-					</div>
-					<div class='form-group'>
-					<label class='col-xs-4 control-label'>Feed Convertion Ratio</label>
-					<label class='col-xs-7 control-label' style='text-align:left;' id='fcr_add'></label>
-					</div>
-					<div class='form-group'>
-					<label class='col-xs-4 control-label'>Mortalitas</label>
-					<div class='col-xs-7'>
-					<input name='mortalitas' id='mortalitas_add' type='number' step=0.0001 min=0 class='form-control' required>
-					</div>
-					</div>
-					<div class='form-group'>
-					<label class='col-xs-4 control-label'>Feed Intake</label>
-					<div class='col-xs-7'>
-					<input name='feed' id='feed_add' type='number' step=0.0001 min=0 class='form-control' required>
-					</div>
-					</div>
-					<div class='form-group'>
-					<label class='col-xs-4 control-label'>Umur</label>
-					<div class='col-xs-7'>
-					<input name='age' id='age_add' type='number' step=0.0001 min=0 class='form-control' required>
-					</div>
-					</div>
-					</div>
-					<div class='modal-footer'>
-					<div class='col-xs-6'>
-					<button class='btn btn-success' type='submit' id='save_add_std_prod'><i class='fa fa-save'></i> Save</button>
-					</div>
-					<div class='col-xs-6 push-left'>
-					<button class='btn btn-danger push-left' type='button' data-dismiss='modal'><i class='fa fa-times'></i> Cancel</button>
-					</div>
-					</div>
-					</form>";
-				} 
-				else
-				{
-					$fcr = $this->input->post('feed') / ($this->input->post('berat_badan') * 1000);
-					$production_data_add = array(
-						'berat_badan' => $this->input->post('berat_badan'),
-						'mortalitas' => $this->input->post('mortalitas'),
-						'feed_intake' => $this->input->post('feed'),
-						'age' => $this->input->post('age'),
-						'fcr' => $fcr,
-						);
-					echo($this->ProductionModel->add($production_data_add) ? 'SUCCESS' : '!SUCCESS');
-				}
-				
-			}
-			else
-			{
-				$this->_show_interface('Unauthorize', '');
-			}
+			echo "
+			<div class='modal-header'>
+			<h1 class='modal-title'>Add Standard Production</h1>
+			</div>
+			<form class='form-horizontal' method='post' id='add_form_std_prod'>
+			<div class='modal-body'>
+			<div class='form-group'>
+			<label class='col-xs-4 control-label'>Berat</label>
+			<div class='col-xs-7'>
+			<input name='berat_badan' id='berat_add' type='number' step=0.0001 min=0 class='form-control' required>
+			</div>
+			</div>
+			<div class='form-group'>
+			<label class='col-xs-4 control-label'>Feed Convertion Ratio</label>
+			<label class='col-xs-7 control-label' style='text-align:left;' id='fcr_add'></label>
+			</div>
+			<div class='form-group'>
+			<label class='col-xs-4 control-label'>Mortalitas</label>
+			<div class='col-xs-7'>
+			<input name='mortalitas' id='mortalitas_add' type='number' step=0.0001 min=0 class='form-control' required>
+			</div>
+			</div>
+			<div class='form-group'>
+			<label class='col-xs-4 control-label'>Feed Intake</label>
+			<div class='col-xs-7'>
+			<input name='feed' id='feed_add' type='number' step=0.0001 min=0 class='form-control' required>
+			</div>
+			</div>
+			<div class='form-group'>
+			<label class='col-xs-4 control-label'>Umur</label>
+			<div class='col-xs-7'>
+			<input name='age' id='age_add' type='number' step=0.0001 min=0 class='form-control' required>
+			</div>
+			</div>
+			</div>
+			<div class='modal-footer'>
+			<div class='col-xs-6'>
+			<button class='btn btn-success' type='submit' id='save_add_std_prod'><i class='fa fa-save'></i> Save</button>
+			</div>
+			<div class='col-xs-6 push-left'>
+			<button class='btn btn-danger push-left' type='button' data-dismiss='modal'><i class='fa fa-times'></i> Cancel</button>
+			</div>
+			</div>
+			</form>";
+		} 
+		elseif($this->_access && !empty($this->input->post('berat_badan')))
+		{
+			$fcr = $this->input->post('feed') / ($this->input->post('berat_badan') * 1000);
+			$production_data_add = array(
+				'berat_badan' => $this->input->post('berat_badan'),
+				'mortalitas' => $this->input->post('mortalitas'),
+				'feed_intake' => $this->input->post('feed'),
+				'age' => $this->input->post('age'),
+				'fcr' => $fcr,
+				);
+			echo($this->ProductionModel->add($production_data_add) ? 'SUCCESS' : '!SUCCESS');
 		}
 		else
 		{
-			redirect(base_url());
+			echo "!LOGIN";
 		}
 	}
 
 	/**
 	 * delete method
 	 */
-	public function delete($id)
+	public function delete($id = '')
 	{
-		if ($this->is_login)
+		if ($this->_access)
 		{
-			$access = $this->auth->privileges_read('standard_delete');
-			if ($access)
-			{
-				empty($id) ? redirect(base_url('/std_production')) : $this->ProductionModel->delete($id) && redirect(base_url('/std_production'));
-			}
-			else
-			{
-				$this->_show_interface('Unauthorize', '');
-			}
+			(empty($id) ? redirect(base_url('/std_production')) : $this->ProductionModel->delete($id) && redirect(base_url('/std_production')));
 		}
 		else
 		{
@@ -263,14 +227,6 @@ class Std_production extends MX_Controller
 			$this->load->view('head');
 			$this->load->view('navbar');
 			$this->load->view($page, $data);
-			$this->load->view('sidebar');
-			$this->load->view('foot');
-		}
-		elseif(empty($data))
-		{
-			$this->load->view('head');
-			$this->load->view('navbar');
-			$this->load->view($page);
 			$this->load->view('sidebar');
 			$this->load->view('foot');
 		}
